3d9396a4b9b7399c26b8692508a53aae00950fdd
Chaeho Yu <chaeho1234@naver.com>
2024-02-20 03:05:26 +0900

    Feat: Header 개발 완료

---
+++ src/components/Header.tsx
@@ -0,0 +1,51 @@
+import styled from "styled-components/native";
+import Enter_icon from "../assets/img/arrow_back_ios.svg"
+import ArrowIcon from "../assets/img/arrow_ios.svg";
+import React from "react";
+interface HeaderProps {
+    title: string;
+}
+
+const Header = ({title}:HeaderProps) => {
+    return (
+        <HeaderContainer>
+            <ArrowIconComponent>
+                <ArrowIcon/>
+            </ArrowIconComponent>
+            <ScreenTitle>{title}</ScreenTitle>
+            <EmtpyView/>
+        </HeaderContainer>
+    );
+};
+
+
+const HeaderContainer = styled.View`
+  width: 100%;
+  height: 30px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+`;
+
+const EmtpyView = styled.View`
+  width: 24px;
+  height: 24px;
+`;
+
+const ScreenTitle = styled.Text`
+  color: #232627;
+  font-family: SUIT;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  letter-spacing: -0.408px;
+ //position: absolute;
+`;
+
+const ArrowIconComponent = styled.TouchableOpacity`
+  width: 24px; height: 24px;
+`
+
+export default Header;
